b88891be01de88fcfbe08d427539cb55af5c9a38
Jean-Luc Deprez <spangaer@users.noreply.github.com>
2024-06-23 13:03:41 +0000

    Cython bindings

--- xr-rs/build.rs
+++ xr-rs/build.rs
@@ -5,7 +5,7 @@ fn main() {
     let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
 
     cbindgen::Builder::new()
-        .with_crate(crate_dir)
+        .with_crate(crate_dir.clone())
         .with_language(cbindgen::Language::C)
         .generate()
         .map_or_else(
@@ -15,6 +15,20 @@ fn main() {
             },
             |bindings| {
                 bindings.write_to_file("target/include/bindings.h");
+            },
+        );
+
+    cbindgen::Builder::new()
+        .with_crate(crate_dir.clone())
+        .with_language(cbindgen::Language::Cython)
+        .generate()
+        .map_or_else(
+            |error| match error {
+                cbindgen::Error::ParseSyntaxError { .. } => {}
+                e => panic!("{:?}", e),
+            },
+            |bindings| {
+                bindings.write_to_file("target/include/bindings.py");
             },
         );
 }
